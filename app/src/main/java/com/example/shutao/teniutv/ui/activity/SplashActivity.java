c62cab3a4424034e0b91237618ce71e999f9fc82
origin <https://github.com/daxia4444/TeNiuTv.git>
2018-02-09 18:35:25 +0800

    '草稿备份'

--- app/src/main/java/com/example/shutao/teniutv/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/shutao/teniutv/ui/activity/SplashActivity.java
@@ -1,16 +1,131 @@
 package com.example.shutao.teniutv.ui.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.example.shutao.teniutv.R;
 
+import butterknife.ButterKnife;
+
 public class SplashActivity extends Activity {
+
+
+    protected Bundle savedInstanceState;
+    public Handler mhandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.savedInstanceState = savedInstanceState;
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        translucentStatusBar(this, false);//
+
+        //initView();
+    }
+
+
+    public void initView() {
+
+
+        mhandler = new Handler();
+        mhandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+
+    public void translucentStatusBar(Activity activity, boolean hideStatusBarBackground) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            LollipopTranslucentStatusBar(activity, hideStatusBarBackground);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            KitKatTranslucentStatusBar(activity);
+        }
+    }
+
+    public void LollipopTranslucentStatusBar(Activity activity, boolean hideStatusBarBackground) {
+        Window window = activity.getWindow();
+        //添加Flag把状态栏设为可绘制模式
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        if (hideStatusBarBackground) {
+            //如果为全透明模式，取消设置Window半透明的Flag
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //设置状态栏为透明
+            window.setStatusBarColor(Color.TRANSPARENT);
+            //设置window的状态栏不可见
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } else {
+            //如果为半透明模式，添加设置Window半透明的Flag
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //设置系统状态栏处于可见状态
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        }
+        //view不根据系统窗口来调整自己的布局
+        ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
+        View mChildView = mContentView.getChildAt(0);
+        if (mChildView != null) {
+            ViewCompat.setFitsSystemWindows(mChildView, false);
+            ViewCompat.requestApplyInsets(mChildView);
+        }
+    }
+
+
+    public void KitKatTranslucentStatusBar(Activity activity) {
+        Window window = activity.getWindow();
+        //设置Window为透明
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        ViewGroup mContentView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
+        View mContentChild = mContentView.getChildAt(0);
+
+        //移除已经存在假状态栏则,并且取消它的Margin间距
+
+        ViewGroup mDecorView = (ViewGroup) window.getDecorView();
+
+        View fakeView = mDecorView.findViewWithTag("statusBarView");
+        if (fakeView != null) {
+            mDecorView.removeView(fakeView);
+        }
+
+        if (mContentChild == null) {
+            return;
+        }
+        if ("marginAdded".equals(mContentChild.getTag())) {
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mContentChild.getLayoutParams();
+            lp.topMargin -= getStatusBarHeight(activity);
+            mContentChild.setLayoutParams(lp);
+            mContentChild.setTag(null);
+        }
+
+        if (mContentChild != null) {
+            //fitsSystemWindow 为 false, 不预留系统栏位置.
+            ViewCompat.setFitsSystemWindows(mContentChild, false);
+        }
+    }
+
+
+    public int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resId > 0) {
+            result = context.getResources().getDimensionPixelOffset(resId);
+        }
+        return result;
     }
 
 }
